5bba4cb3e03a7d67aa68056ced2d782b8469e073
Ayaan Imtiaz <ayaanimtiazflm@gmail.com>
2026-09-04 09:31:38 -0500

    Time: 167 ms (5.01%), Space: 36.2 MB (5.36%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,23 +1,13 @@
 from collections import deque
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # make a stack with a temp and an index
-        # check -2  of stack
-
-
-        stack_temp = deque()
-        result = [0] * len(temperatures)
-        curr_elm = 0
-
-        # we hvae to loop thru
-        # for loop with enumerate
-        # while the stack does exist and the current temperature is higher than the top of the stack
-        # pop and place to result
         
+        stack = deque()
+        result = [0] * len(temperatures)
 
-        for index, current_temp in enumerate(temperatures):
-            while stack_temp and current_temp > stack_temp[-1][0]:
-                result[stack_temp[-1][1]] = index - stack_temp[-1][1]
-                stack_temp.pop()
-            stack_temp.append([current_temp, index])
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > stack[-1][0]:
+                result[stack[-1][1]] = i - stack[-1][1]
+                stack.pop()
+            stack.append([temperatures[i], i])
         return result
